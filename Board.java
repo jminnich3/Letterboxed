ebb4023e4caf27228bcd0201665318b2c69bf767
Michael Chambers <geofreypillagefish@gmail.com>
2025-02-25 19:40:47 -0500

    added board code

--- Board.java
+++ Board.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Board {
 
     /*
@@ -14,6 +16,71 @@ Board class
     return number of letters left to use
     */
 
+    private int sideLength;
+    private char[][] board;
+    ArrayList<Character> unusedLetters;
 
+    /**
+     *
+     */
+    public Board(int sideLength, String lineFromFile)
+    {
+        this.sideLength = sideLength;
+        unusedLetters = new ArrayList<Character>();
+        board = new char[4][sideLength];
+
+        int boardRow = 0;
+        int boardCol = 0;
+        for(int i = 0; i < lineFromFile.length(); i++)
+        {
+            if(lineFromFile.charAt(i) == ' ')
+            {
+                boardRow++;
+                boardCol = 0;
+            }
+            else
+            {
+                unusedLetters.add(lineFromFile.charAt(i));
+                board[boardRow][boardCol] = lineFromFile.charAt(i);
+                boardCol++;
+            }
+        }
+    }
+
+    public int getUnusedCount()
+    {
+        return unusedLetters.size();
+    }
+
+    public void printFormattedBoard()
+    {
+        //print top row
+        System.out.print(" ");
+        for(int i = 0; i < board[0].length; i++)
+        {
+            System.out.print(board[0][i] + " ");
+        }
+        System.out.println();
+
+        //print left and right sides
+        for(int i = 0; i < board[0].length; i++)
+        {
+            System.out.print(board[1][i]);
+            for(int j = 0; j < sideLength - 1; j++)
+            {
+                System.out.print("  ");
+            }
+            System.out.print(" ");
+            System.out.print(board[2][i]);
+            System.out.println();
+        }
+
+        //print bottom row
+        System.out.print(" ");
+        for(int i = 0; i < board[0].length; i++)
+        {
+            System.out.print(board[3][i] + " ");
+        }
+    }
 
 }
---
+++ BoardTester.java
@@ -0,0 +1,6 @@
+public class BoardTester {
+    public static void main(String[] args) {
+        Board myBoard = new Board(4, "irly hope this work");
+        myBoard.printFormattedBoard();
+    }
+}
